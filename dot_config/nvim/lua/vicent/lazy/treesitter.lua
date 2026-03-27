c9a9f5b686bf36d5c93738c72a1a440c1a643927
Vicent Gozalbes <vigosan@gmail.com>
2026-03-27 11:12:23 +0100

    Fix nvim-treesitter config for new API

--- dot_config/nvim/lua/vicent/lazy/treesitter.lua
+++ dot_config/nvim/lua/vicent/lazy/treesitter.lua
@@ -2,14 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require 'nvim-treesitter.configs'.setup {
+    require('nvim-treesitter').setup {
       auto_install = true,
-      highlight = {
-        enable = true
-      },
-      indent = {
-        enable = true
-      }
     }
   end
 }
